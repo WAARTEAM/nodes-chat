6d223cf1d09682c668f21eadeba30012d830f0dd
MohamedBelkheirRBK <mohamedbel@protonmail.com>
2020-01-08 18:47:15 +0100

    testable authorization middleware

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const http = require('http').Server(app)
+const cors = require('cors');
+var io = require('socket.io')(http);
+const auth = require('./auth');
 
+app.use(cors())
 app.use(express.json());
+app.use(auth.middleware);
+
+io.use(auth.socketMiddleware)
 
 const port = process.env.PORT || 5001
 
-var server = app.listen(port, () => {
-    console.log('server is running on port', server.address().port);
+http.listen(port, ()=> {
+    console.log(`server running at ${port}`)
 })
