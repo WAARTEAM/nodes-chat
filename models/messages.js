61d43c378bb261c3562d465137ea36221ecb4569
Hammam Elmuratdh <47317019+Hamam-Elmuratdh@users.noreply.github.com>
2020-01-13 17:50:36 +0100

    comments added

--- models/messages.js
+++ models/messages.js
@@ -9,13 +9,13 @@ con.query(MessagesSchema, (err, data) => {
   if (err) console.error(err);
   else console.log("messages Table IS UP");
 });
-
+/// insert the message with the chatroom id and the data, takes the data and the chat room id of the message
 function sendMessage(data, chatroom_id) {
   return con.query(
     `INSERT into messages (data,chatroom_id) VALUES ('${data}', '${chatroom_id}')`
   );
 }
-
+//  select the message in thedata base based on the chatroom id 
 function checkMessage(chatroom_id) {
   return con.query(
     `SELECT * FROM messages WHERE chatroom_id  = '${chatroom_id}'`
