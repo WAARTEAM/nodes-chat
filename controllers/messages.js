923f020686971e881de97469c633e01bb0500f8e
Hammam Elmuratdh <47317019+Hamam-Elmuratdh@users.noreply.github.com>
2020-01-13 17:52:21 +0100

    Update messages.js

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,5 +1,5 @@
 const messagesModel = require("../models/messages");
-
+// pass parameter to the send message function 
 function Send(data, chatroom_id) {
   return messagesModel
     .sendMessage(data, chatroom_id)
@@ -10,6 +10,7 @@ function Send(data, chatroom_id) {
       throw err;
     });
 }
+// pass the chat room id  to check messages function 
 function Fetch(chatroom_id) {
   return messagesModel
     .checkMessage(chatroom_id)
